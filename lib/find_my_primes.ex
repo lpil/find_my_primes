000d1df289646d162b0aaa512277aa6bc92b2dab
Louis Pilfold <louis@lpil.uk>
2016-12-03 20:31:15 +0000

    FindMyPrimes.table sync call with dynamic workers

--- lib/find_my_primes.ex
+++ lib/find_my_primes.ex
@@ -1,20 +1,65 @@
 defmodule FindMyPrimes do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
+  defdelegate table(n), to: FindMyPrimes.Manager
+
+  #
+  # Callbacks
+  #
+
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
-
-    # Define workers and child supervisors to be supervised
     children = [
-      # Starts a worker by calling: FindMyPrimes.Worker.start_link(arg1, arg2, arg3)
-      # worker(FindMyPrimes.Worker, [arg1, arg2, arg3]),
+      supervisor(Task.Supervisor, [[name: FindMyPrimes.TaskSupervisor]]),
+      worker(FindMyPrimes.Manager, [[name: FindMyPrimes.Manager]])
     ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: FindMyPrimes.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+end
+
+defmodule FindMyPrimes.Manager do
+  use GenServer
+  alias FindMyPrimes.{Worker, TaskSupervisor}
+
+  def start_link(opts) do
+    name = opts[:name]
+    GenServer.start_link(__MODULE__, [], name: name)
+  end
+
+
+  def table(n) when is_integer(n) and n >= 1 do
+    GenServer.call(__MODULE__, {:table, n})
+  end
+
+  #
+  # Callbacks
+  #
+
+  def init(_) do
+    {:ok, []}
+  end
+
+  def handle_call({:table, n}, from, state) do
+    Task.Supervisor.start_child(TaskSupervisor, fn->
+      Worker.reply_with_table(n, from)
+    end)
+    {:noreply, state}
+  end
+end
+
+
+defmodule FindMyPrimes.Worker do
+  alias FindMyPrimes.{Prime, MultTable}
+
+  def reply_with_table(num, target) do
+    GenServer.reply(target, table(num))
+  end
+
+  defp table(num) do
+    num
+    |> Prime.list
+    |> Enum.reverse
+    |> MultTable.draw
   end
 end
--- test/find_my_primes_test.exs
+++ test/find_my_primes_test.exs
@@ -2,7 +2,14 @@ defmodule FindMyPrimesTest do
   use ExUnit.Case
   doctest FindMyPrimes
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "" do
+    test "the truth" do
+      assert FindMyPrimes.table(3) == """
+      |    |  2 |  3 |  5 |
+      |  2 |  4 |  6 | 10 |
+      |  3 |  6 |  9 | 15 |
+      |  5 | 10 | 15 | 25 |
+      """
+    end
   end
 end
